a6923bbd9496c640df559ef232efde20b4350af4
febydahlan <febbydahlan034@gmail.com>
2019-11-18 14:18:57 +0700

    First Commit

---
+++ app/src/main/java/com/example/android/shushme/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.android.shushme;
+
+/*
+* Copyright (C) 2017 The Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*  	http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Constants
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private static final int AUTOCOMPLETE_REQUEST_CODE = 1;
+
+    // Member variables
+    private PlaceListAdapter mAdapter;
+    private RecyclerView mRecyclerView;
+
+    /**
+     * Called when the activity is starting
+     *
+     * @param savedInstanceState The Bundle that contains the data supplied in onSaveInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize the SDK
+        Places.initialize(getApplicationContext(), "AIzaSyBAg3Qcdl-QElDrhKyUtnf3BOYPlZDsv9c");
+
+        // Create a new Places client instance
+        PlacesClient placesClient = Places.createClient(this);
+
+
+        // Set up the recycler view
+        mRecyclerView = (RecyclerView) findViewById(R.id.places_list_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mAdapter = new PlaceListAdapter(this);
+        mRecyclerView.setAdapter(mAdapter);
+
+    }
+
+    public void addNewLocation(View view) {
+        // Set the fields to specify which types of place data to
+        // return after the user has made a selection.
+        List<Place.Field> fields = Arrays.asList(Place.Field.ID, Place.Field.NAME);
+
+        // Start the autocomplete intent.
+        Intent intent = new Autocomplete.IntentBuilder(
+                AutocompleteActivityMode.FULLSCREEN, fields)
+                .build(this);
+        startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE);
+    }
+}
